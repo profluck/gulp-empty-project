182163d15967d25f97539b07942f99e4400700ed
Arthur Danko <nullbool@hotmail.com>
2019-01-11 22:46:37 +0200

    Add base structure of app

---
+++ README.md
@@ -0,0 +1,7 @@
+<b>Repository link: </b>git clone https://github.com/profluck/gulp-empty-project.git
+<br/>
+<b>If gulp is not installed globally:</b> npm i gulp --global
+<br/>
+<b>AND than use</b> npm install
+<br/><br/>
+<img src="app/images/main.png"/>
---
+++ app/js/index.js
@@ -0,0 +1,16 @@
+/**
+ * Third party
+ */
+//= ../../node_modules/jquery/dist/jquery.js
+/**
+ * Bootstrap v4
+ * //= ../../node_modules/bootstrap/dist/js/bootstrap.js
+ */
+//= ../../node_modules/bootstrap3/dist/js/bootstrap.js
+
+
+/**
+ * Custom
+ */
+
+//= partials/app.js
